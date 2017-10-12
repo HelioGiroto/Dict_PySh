fc32a851582e0fce05ca1d500c5bc986f0b06e77
Helio Giroto <31024350+HelioGiroto@users.noreply.github.com>
2017-10-12 13:15:26 -0300

    Update cat.py

--- cat.py
+++ cat.py
@@ -1,5 +1,13 @@
+
+######################################################################
+
+#       ESTE PROGRAMA PYTHON SIMULA O COMANDO CAT DO LINUX           #
+
+######################################################################
+
+
 arq='frutas.txt'
-with open(arq, 'r') as arquivo:
-    for linha in arquivo:
-        print(linha)
-        #input()
+with open(arq, 'r') as arquivo:     # Abre o arquivo escolhido na linha acima...
+    for linha in arquivo:           # Para cada linha neste arquivo...
+        print(linha)                # Imprime linha
+        #input()                    # Pode descomentar para dar uma pausa a cada linha impressa...
