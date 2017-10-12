afd6fdc79a7aab3cc6ec2a4957676f7a5415854c
Helio Giroto <31024350+HelioGiroto@users.noreply.github.com>
2017-10-12 13:13:11 -0300

    Create cut.py

---
+++ cut.py
@@ -0,0 +1,14 @@
+######################################################################
+
+#       ESTE PROGRAMA PYTHON SIMULA O COMANDO CUT DO LINUX           #
+
+######################################################################
+
+
+arq='frutas.txt'                      # Escolhe o arquivo
+
+with open(arq, 'r') as arquivo:
+    for linha in arquivo:             # A cada linha do arquivo...
+        preco=linha.split('	')[1]     # Divide (separa) a linha em varias partes usando o delimitador TAB ('  ') e captura apenas o campo número [1] - ou seja o 2º do array
+        print(preco)                  # Imprime esse campo (o campo preço)
+
