10be2aca65e315303ff871ad0c07f7abbfb76a46
Helio Giroto <31024350+HelioGiroto@users.noreply.github.com>
2017-10-12 13:19:19 -0300

    Update cut.py

--- cut.py
+++ cut.py
@@ -12,3 +12,7 @@ with open(arq, 'r') as arquivo:
         preco=linha.split('	')[1]     # Divide (separa) a linha em varias partes usando o delimitador TAB ('  ') e captura apenas o campo número [1] - ou seja o 2º do array
         print(preco)                  # Imprime esse campo (o campo preço)
 
+'''
+O Arquivo frutas.txt pode ser visualizado em:
+https://raw.githubusercontent.com/HelioGiroto/Dict_PySh/master/frutas.txt
+'''
