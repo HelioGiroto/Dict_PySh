8475e5e90e1a2acff8bcd77e717169d3996efb28
Helio Giroto <31024350+HelioGiroto@users.noreply.github.com>
2017-10-13 22:03:57 -0300

    Update corta_colunas_tipo_awk.py

--- corta_colunas_tipo_awk.py
+++ corta_colunas_tipo_awk.py
@@ -2,23 +2,20 @@ planetas=["Marte", "Jupiter", "Mercurio", "Venus", "Saturno"]
 
 for planeta in planetas:
 	
-	arqFonte=planeta+".csv"			# Concatena variavel para formar nome do arquivo
-	arqGerado=planeta+".txt"
+	arqCSV=planeta+".csv"			# Concatena variável para formar nome dos arquivos
+	arqTXT=planeta+".txt"
 
-#	arqFonte='Marte.csv'
-#	arqSaida=open('marte.txt', 'a')
+	arqFonte=open(arqCSV, 'r')
+	arqSaida=open(arqTXT, 'a')
 
-	arqSaida=open(arqGerado, 'a')
-		
-	with open(arqFonte, 'r') as arquivo:
-		for linha in arquivo:
-			corte1=linha.split(';')[0]	# Pega primeira coluna
-			corte2=corte1.split(' ')[0]	# Faz outro corte do que já cortei acima
-			corteA=linha.split(';')[1]	# Pega segunda coluna
-			corteB=corteA.split("'")[3]	# Faz outro corte na linha acima - Ver *OBS
+	for linha in arqFonte:
+		corte1=linha.split(';')[0]	# Pega primeira coluna
+		corte2=corte1.split(' ')[0]	# Faz outro corte do que já cortei acima
+		corteA=linha.split(';')[1]	# Pega segunda coluna
+		corteB=corteA.split("'")[3]	# Faz outro corte na linha acima - Ver *OBS
 
-			print(corte2, corteB)		# Imprime na tela ao mesmo tempo que grava em arquivo. Ver abaixo:
-			arqSaida.write(corte2 + '-' + corteB + '\n')
+		print(corte2, corteB)		# Imprime na tela ao mesmo tempo que grava em arquivo. Ver abaixo:
+		arqSaida.write(corte2 + '-' + corteB + '\n')
 
 	arqSaida.close()				# Fecha arquivo de escrita (de saida)
 	#arqFonte.close() - Nao precisa fechar arq aberto para leitura!!! Dá erro.
@@ -31,3 +28,5 @@ for planeta in planetas:
 2017/10/12 00:00:00;('Vir', 'Virgo');13:53:23.08;-10:32:04.1;13:54:17.90;-10:37:02.8
 
 """
+
+# Autor: Helio Giroto
